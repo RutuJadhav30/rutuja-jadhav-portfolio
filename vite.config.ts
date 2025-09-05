d625f8629fe39a351ef5b4c41c0ef33adddd7773
Nitin Govardhane <nitingovardhane000@gmail.com>
2025-09-06 04:32:01 +0530

    Setup GitHub Pages deployment with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/rutuja-jadhav-portfolio/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
